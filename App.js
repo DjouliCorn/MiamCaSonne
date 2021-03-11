3d62efde164030449c207d9402fe3fbcf4d4d45b
DjouliCorn <tissier.julie@gmail.com>
2021-03-11 11:47:19 +0100

    refresh appear when slide from top

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, Container } from 'react-native';
+import { StyleSheet, Text, View, Image, Container, SafeAreaView, ScrollView, RefreshControl } from 'react-native';
 import CategorieSlider from './components/categorieSlider';
 import BannSlider from './components/bannSlider';
 
+const wait = (timeout) => {
+  return new Promise(resolve => setTimeout(resolve, timeout));
+}
+
 export default function App() {
+
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <ScrollView 
+      refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+        />
+      }>
       <CategorieSlider style={styles.container} />
       <BannSlider />
-    </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
