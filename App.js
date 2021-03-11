816f8361e81b27d400a6d5ddb5e88bf946a69f0f
NickFingertip <nicolas.maurice@edu.Itescia.fr>
2021-03-11 17:24:39 +0100

    under construction

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ListeRestos from './ListeRestos';
 import PageResto from './PageResto';
 
 export default function App() {
   return (
-    <PageResto />
+    <ListeRestos />
   );
 }
 
---
+++ ListeRestos.js
@@ -0,0 +1,78 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, Dimensions, FlatList, ScrollView, SafeAreaView, TouchableOpacity } from 'react-native';
+import Constants from 'expo-constants';
+import apirestos from './apirestos'
+
+const {width, height} = Dimensions.get('screen');
+const api = apirestos.create();
+
+export default class App extends React.Component {
+
+    state = {
+        listRestos: []
+    }
+    constructor(props){
+        super(props);
+        this.state = {
+            
+            dataSource: []
+        }
+    }
+
+    async componentDidMount(){
+        const listRestosFetched = await api.getRestos()
+        if(listRestosFetched.length>0){
+            this.setState({
+                listRestos: listRestosFetched
+            })
+        }
+        console.log('liste Restos:', JSON.stringify(listRestos))
+    }
+
+    render(){
+        return(
+            <View style={{ flex: 1, paddingTop: 20 }}>
+
+                <FlatList
+                    data={this.state.listRestos}
+                    renderItem={({ item, index }) => <Text>{item[index].name}</Text>}
+                    keyExtractor={(item, index) => index}
+                />
+            </View>
+        )
+    }
+}
+
+/*const DATA = [
+    {
+      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+      title: "First Item",
+    },
+    {
+      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+      title: "Second Item",
+    },
+    {
+      id: "58694a0f-3da1-471f-bd96-145571e29d72",
+      title: "Third Item",
+    },
+  ];
+
+export default () => {
+    
+    return(
+        <View>
+            <StatusBar hidden />
+            <FlatList>
+            
+
+                <Text>
+                    Hello People
+                </Text>
+            
+            </FlatList>
+        </View>
+    )
+    
+    }*/
